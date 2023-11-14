a596ac325ecab92561a1cc8132f7acfe84de7c62
pfmaria13 <pfmaria13@gmail.com>
2023-11-14 19:54:15 +0500

    Первые 10 страниц выполнены. Реализована логика выбора дальнейших действий Элизабет для первой ветки

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ const paper1 = document.querySelector('#p1')
 const paper2 = document.querySelector('#p2')
 const paper3 = document.querySelector('#p3')
 const paper4 = document.querySelector('#p4')
+const paper5 = document.querySelector('#p5')
+const paper6 = document.querySelector('#p6')
+const paper7 = document.querySelector('#p7')
 
 // Event listeners
 prevBtn.addEventListener("click", goPrevious);
@@ -14,7 +17,7 @@ nextBtn.addEventListener("click", goNext);
 
 // Business Logic
 let currentState = 1;
-let numOfPapers = 4;
+let numOfPapers = 7;
 let maxState = numOfPapers + 1;
 
 
@@ -51,9 +54,24 @@ function goNext() {
                 paper3.style.zIndex = 3;
                 break;
             case 4:
-                closeBook(false);
                 paper4.classList.add("flipped");
                 paper4.style.zIndex = 4;
+                paper5.style.zIndex = 4;
+                break;
+            case 5:
+                paper5.classList.add("flipped");
+                paper5.style.zIndex = 5;
+                paper6.style.zIndex = 5;
+                break;
+            case 6:
+                paper6.classList.add("flipped");
+                paper6.style.zIndex = 6;
+                paper7.style.zIndex = 7;
+                break;
+            case 7:
+                closeBook(false);
+                paper7.classList.add("flipped");
+                paper7.style.zIndex = 7;
                 break;
             default:
                 throw new Error("unknown state");
@@ -69,23 +87,44 @@ function goPrevious() {
             case 2:
                 closeBook(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 4;
+                paper1.style.zIndex = 7;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 3;
+                paper2.style.zIndex = 6;
                 break;
             case 4:
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 2;
+                paper3.style.zIndex = 5;
                 break;
             case 5:
-                openBook()
                 paper4.classList.remove("flipped");
-                paper4.style.zIndex = 1;
+                paper4.style.zIndex = 4;
+                paper5.style.zIndex = 3;
+                break;
+            case 6:
+                paper5.classList.remove("flipped");
+                paper5.style.zIndex = 4;
+                paper6.style.zIndex = 3;
+                break;
+            case 7:
+                paper6.classList.remove("flipped");
+                paper6.style.zIndex = 5;
+                paper7.style.zIndex = 1;
+                break;
+            case 8:
+                openBook()
+                paper7.classList.remove("flipped");
+                paper7.style.zIndex = 6;
                 break;
         }
-
         currentState--;
     }
+}
+
+function chooseYakov() {
+    paper5.classList.add("flipped");
+    paper5.style.zIndex = 5;
+    paper6.style.zIndex = 5;
+    prevBtn.style.visibility = 'hidden';
 }
